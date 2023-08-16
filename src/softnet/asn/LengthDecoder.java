32c3d028c1394a24f849232551db18cac76ebc44
Robert Koifman <robert.koifman@gmail.com>
2023-08-16 18:43:01 +0500

    The license terms updated to Apache License, Version 2.0

--- src/softnet/asn/LengthDecoder.java
+++ src/softnet/asn/LengthDecoder.java
@@ -1,31 +1,23 @@
 /*
-*	Copyright (c) 2023 Robert Koifman 
-*	
-*	This file is part of Softnet ASN.1 Codec (Java).
-*	
-*	Softnet ASN.1 Codec (Java) is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
-*	
-*	Softnet ASN.1 Codec (Java) is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*	GNU General Public License for more details.
-*	
-*	You should have received a copy of the GNU General Public License
-*	along with Softnet ASN.1 Codec (Java). If not, see <https://www.gnu.org/licenses/>.
+*	Copyright 2023 Robert Koifman
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
 */
 
 package softnet.asn;
 
 class LengthDecoder
 {
-    public static PairInt32 decode2(byte[] buffer, int offset) throws FormatAsnException
-    {
-    	return null;
-    }
-    
     public static void decode(byte[] buffer, int offset, PairInt32 result) throws FormatAsnException
     {
     	try
